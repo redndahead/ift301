12752bf271b508f3adc8c5eb6f8a73c13d47d75d
Adam Moore <redndahead@gmail.com>
2018-09-05 07:47:39 -0700

    Final: Change images to jpg from webp.

--- final/js/home-carousel.js
+++ final/js/home-carousel.js
@@ -1,5 +1,4 @@
 $('#home-carousel').on('slide.bs.carousel', function (event) {
   $("#carousel-thumbnails li").removeClass("active");
   $("#carousel-thumbnails li:eq(" + event.to + ")").addClass("active");
-  console.log(event);
 });
